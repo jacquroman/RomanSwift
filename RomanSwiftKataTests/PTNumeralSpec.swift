3314661d5ce13fdb1bf7f41689aa1d08bd65a111
daniel ogorek <acosk8er@yahoo.com>
2016-03-29 09:36:34 -0400

    moved PTNumeral declaration outside of "it" statements

--- RomanSwiftKataTests/PTNumeralSpec.swift
+++ RomanSwiftKataTests/PTNumeralSpec.swift
@@ -16,21 +16,20 @@ class PTNumeralSpec: QuickSpec {
     override func spec() {
         
         describe("PTNumeral") {
-            context("when numeral entered", { 
+            let numeral = PTNumeral()
+            
+            context("when numeral entered", {
                 it("should return 1 when I entered") {
-                    let numeral = PTNumeral()
                     let arabic = numeral.getArabicForNumeral("I")
                     expect(arabic).to(equal(1))
                 }
                 
                 it("should return 2 when II entered") {
-                    let numeral = PTNumeral()
                     let arabic = numeral.getArabicForNumeral("II")
                     expect(arabic).to(equal(2))
                 }
                 
                 it("should return 5 when V entered") {
-                    let numeral = PTNumeral()
                     let arabic = numeral.getArabicForNumeral("V")
                     expect(arabic).to(equal(5))
                 }
